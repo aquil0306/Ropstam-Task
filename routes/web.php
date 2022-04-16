448276fb43c543e1a0f0df226f44a6a78b911632
aquil0306 <muhammadaqeelprivate@gmail.com>
2022-04-16 13:51:31 +0500

    P

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/create', 'testController@create');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/test', 'HomeController@test')->name('test');
-
-Route::get('/challan', 'TestController@challan')->name('challan');
-Route::get('/delete_challan', 'TestController@delete_challan')->name('delete_challan');
-
-Route::get('/generate_challan', 'TestController@generate_challan')->name('generate_challan');
-
-Route::post('/create_challan', 'TestController@create_challan')->name('create_challan');
-Route::get('/consession', 'TestController@consession')->name('consession');
-
-Route::get('/zoom', 'ZoomController@zoom')->name('zoom');
+ 
